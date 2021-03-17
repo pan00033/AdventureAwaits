09d217c03288bf39e23dc779a2075309a32e4647
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-16 20:21:30 -0500

    added campground delete functionality; completed basic CRUD

--- app.js
+++ app.js
@@ -59,6 +59,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
+
 
 
 // app.get('/makecampground', async (req, res) => {

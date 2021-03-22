88c8a37d80368b1ba2cb239efdbf295afac56e74
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-22 10:27:51 -0500

    adding an author to campground

--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6057ebf6fbd7f65f7c891f23',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',

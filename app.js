25ba8fc854055377a4e67ec2b72faf105ef3b967
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-21 19:54:54 -0500

    adding register form

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campground');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campground');
+const reviewRoutes = require('./routes/reviews');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -65,8 +67,9 @@ app.use((req, res, next) => {
 });
 
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get("/", (req, res) => {
     res.render('home');

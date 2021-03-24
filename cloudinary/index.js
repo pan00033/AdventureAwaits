48bac59b18220178f3d44d8789334d1ef77c2ff0
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-23 20:13:39 -0500

    storing uploaded image links in mongo

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary,
-    folder: 'AdventureAwaits',
-    allowedFormats: ['jpeg', 'png', 'jpg']
+    params: {
+        folder: 'AdventureAwaits',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
 });
 
 module.exports = {

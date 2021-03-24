76dcf56905348e3142b17c3b0778b496d70fcf91
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-23 21:17:56 -0500

    fixing seeds data

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,22 @@ const seedDB = async () => {
             author: '6058f418c47d9b27e852be6b',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis mollitia culpa eaque. Illum deleniti magni sequi iste nostrum debitis amet odit, aliquam architecto ut libero officia corporis dolor quisquam deserunt.',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dmbn8ucfc/image/upload/v1616551955/AdventureAwaits/ajnuoli4jr0dob56ysrz.jpg',
+                    filename: 'AdventureAwaits/ajnuoli4jr0dob56ysrz'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dmbn8ucfc/image/upload/v1616551955/AdventureAwaits/xwsvlv7hnrouhwotbjx6.jpg',
+                    filename: 'AdventureAwaits/xwsvlv7hnrouhwotbjx6'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dmbn8ucfc/image/upload/v1616551991/AdventureAwaits/l4uvkgysdc2xjiw5rraq.jpg',
+                    filename: 'AdventureAwaits/l4uvkgysdc2xjiw5rraq'
+                }
+            ]
         });
         await camp.save();
     }

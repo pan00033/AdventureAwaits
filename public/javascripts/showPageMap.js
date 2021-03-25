d22c4e4185d83e9bfaef674051b087245ebc6fa2
ArizonaPeachTea <zuxingp888@gmail.com>
2021-03-25 16:32:13 -0500

    reseeding database

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
